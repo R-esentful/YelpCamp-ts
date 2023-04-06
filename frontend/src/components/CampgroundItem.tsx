c5a066274b4e0f9d46da14a7dd12334178b7f7db
R-esentful <raneljohnt@gmail.com>
2023-04-06 19:46:37 +0800

    Updated components

---
+++ frontend/src/components/CampgroundItem.tsx
@@ -0,0 +1,59 @@
+import {
+  GiCampfire,
+  GiShower,
+  GiElectric,
+  GiHiking,
+  GiBoatFishing,
+  GiRoundStar,
+} from "react-icons/gi";
+import { TbSwimming } from "react-icons/tb";
+
+function CampgroundItem({ img }: any) {
+  return (
+    <div className="hover:cursor-pointer">
+      <div className="w-full">
+        <img src={img} alt="" className="rounded-xl w-full h-[260px]" />
+      </div>
+      <div className="p-1">
+        <header className="flex justify-between mb-2">
+          <p className="text-[14px]">Name</p>
+          <div className="flex">
+            <GiRoundStar size={12} className="m-auto mr-1" />
+            <p className="text-[12px] m-auto">4.9</p>
+          </div>
+        </header>
+        <div className="flex justify-between mb-2">
+          <p className="text-[12px] mr-2">Amenities</p>
+          <ul className="flex">
+            <li className="mr-1">
+              <GiCampfire />
+            </li>
+            <li className="mr-1">
+              <GiShower />
+            </li>
+            <li className="mr-1">
+              <GiElectric />
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex justify-between mb-1">
+          <p className="text-[12px] mr-2">Activities</p>
+          <ul className="flex ">
+            <li className="mr-1">
+              <GiHiking />
+            </li>
+            <li className="mr-1">
+              <GiBoatFishing />
+            </li>
+            <li className="mr-1">
+              <TbSwimming />
+            </li>
+          </ul>
+        </div>
+        <p>Free</p>
+      </div>
+    </div>
+  );
+}
+export default CampgroundItem;
