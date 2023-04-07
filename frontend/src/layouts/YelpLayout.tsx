3e580380366bb90c63149a377dca3ab68eb5773f
R-esentful <raneljohnt@gmail.com>
2023-04-07 21:48:44 +0800

    Updated layout

--- frontend/src/layouts/YelpLayout.tsx
+++ frontend/src/layouts/YelpLayout.tsx
@@ -1,9 +1,11 @@
 import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
-import { FaUser } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
 import { GiCampingTent } from "react-icons/gi";
+import { TbLogout } from "react-icons/tb";
 import logo from "../assets/logo.png";
 import landing from "../assets/landing-hero.png";
 import sample from "../assets/sample.jpg";
+
 function LandingLayout() {
   const location = useLocation();
   const navigate = useNavigate();
@@ -12,12 +14,19 @@ function LandingLayout() {
     location.pathname === "/" ||
     location.pathname === "/register" ||
     location.pathname === "/login";
-  const handleNavigate = () => {
-    navigate("/");
+
+  const handleNavigate = (where: string) => {
+    if (where === "home") {
+      navigate("/");
+    }
+    if (where === "profile") {
+      navigate("/profile");
+    }
   };
+
   return (
     <>
-      <div className="h-screen flex flex-col p-[10px]">
+      <div className="h-screen flex flex-col p-[10px]" id="Home">
         <header
           className={`h-[50px] flex justify-between ${
             campground
@@ -25,7 +34,7 @@ function LandingLayout() {
               : " border-b-[1px] border-solid border-gray-200 fixed top-0 left-0 w-full bg-white z-10   "
           }`}
         >
-          <section className="flex hover:cursor-pointer" onClick={handleNavigate}>
+          <section className="flex hover:cursor-pointer" onClick={() => handleNavigate("home")}>
             <img src={logo} alt="YelpCamp Logo" className=" w-[50px] h-[50px]" />
             <h1 className="text-[18px] py-[12px] font-[900]">YelpCamp</h1>
           </section>
@@ -33,13 +42,13 @@ function LandingLayout() {
           {location.pathname === "/" ? (
             <>
               <section className="py-[12px]">
-                <a href="#asda" className="m-[1rem]">
+                <a href="#Home" className="m-[1rem]">
                   Home
                 </a>
-                <a href="#asda" className="m-[1rem]">
+                <a href="#About" className="m-[1rem]">
                   About
                 </a>
-                <a href="#asdasd" className="m-[1rem]">
+                <a href="#Contact" className="m-[1rem]">
                   Contact
                 </a>
               </section>
@@ -57,21 +66,27 @@ function LandingLayout() {
             ""
           )}
 
-          {location.pathname === "/campgrounds" || location.pathname === "/campgrounds/new" ? (
+          {location.pathname.includes("/campgrounds") || location.pathname === "/profile" ? (
             <>
               <section className="flex mr-4">
                 <section className="flex mr-4">
-                  <Link to="/campgrounds/new" className="flex text-[12px] my-auto mx-4">
-                    <GiCampingTent size={22} className="mr-2" />
-                    New Campground
+                  <Link to="/campgrounds" className="flex text-[12px] my-auto mx-4">
+                    <FaHome size={20} className="" />
                   </Link>
-                  <Link to="/profile" className="flex text-[12px] my-auto mx-4">
-                    <FaUser size={16} className="mr-2" />
-                    Profile
+
+                  <Link to="/campgrounds/new" className="flex text-[12px] my-auto mx-4">
+                    <GiCampingTent size={22} className="" />
+                  </Link>
+
+                  <Link to="/" className="flex text-[12px] my-auto mx-4">
+                    <TbLogout size={22} className="" />
                   </Link>
                 </section>
                 <section className="flex flex-col justify-center">
-                  <section className="avatar">
+                  <section
+                    className="avatar hover:cursor-pointer"
+                    onClick={() => handleNavigate("profile")}
+                  >
                     <div className=" w-[36px] rounded-full">
                       <img src={sample} alt="" />
                     </div>
