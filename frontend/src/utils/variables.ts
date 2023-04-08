96be00648cf979bca196f481599b00b767af0d65
R-esentful <raneljohnt@gmail.com>
2023-04-08 21:14:51 +0800

    Updated utils

--- frontend/src/utils/variables.ts
+++ frontend/src/utils/variables.ts
@@ -1,3 +1,4 @@
 export const name_regex = /^[a-zA-Z]+$/;
 export const email_regex = /^\S+@\S+\.\S+$/;
 export const user_api = "/api/v1/users";
+export const campground_api = "/api/v1/campgrounds";
