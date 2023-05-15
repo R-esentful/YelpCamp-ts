f6a45f03abe464975743a4611b82e99e3aae70aa
R-esentful <raneljohnt@gmail.com>
2023-05-15 18:44:13 +0800

    Updated pages

--- frontend/src/pages/NewCampground.tsx
+++ frontend/src/pages/NewCampground.tsx
@@ -1,15 +1,16 @@
-import { Formik, Field, Form, FormikHelpers } from "formik";
+import { Formik, Field, Form } from "formik";
 import { GiCampfire, GiShower, GiElectric, GiHiking, GiBoatFishing } from "react-icons/gi";
 import { HiLocationMarker, HiOutlineUpload } from "react-icons/hi";
 import { TbSwimming } from "react-icons/tb";
 import { useDropzone } from "react-dropzone";
 
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import ImageItem from "../components/ImageItem";
 
 import Philippines from "../data/Philippines.json";
 import axios from "axios";
 import { campground_api } from "../utils/variables";
+import { upload } from "@testing-library/user-event/dist/upload";
 
 interface FileInterface extends File {
   preview: string;
@@ -26,12 +27,11 @@ interface NewCampInterface {
 
 function NewCampground() {
   const [files, setFiles] = useState<FileInterface[]>([]);
-  const [locationFilter, setLocationFilter] = useState(Philippines);
-  const [selectedLocation, setSelectedLocation] = useState<string>("");
+  const [selectedProvince, setSelectedProvince] = useState<string>("");
 
-  const provinceName = locationFilter.map(({ province }) => province);
-  const provinceCity = locationFilter.find(
-    (province) => province.province === "Cebu"
+  const provinceName = Philippines.map(({ province }) => province);
+  const provinceCity = Philippines.find(
+    (province) => province.province === selectedProvince
   )?.CityOrMunicipalities;
 
   const removeFile = (name: string): void => {
@@ -67,19 +67,13 @@ function NewCampground() {
   };
 
   const handleSubmit = async (values: NewCampInterface) => {
-    const response = await axios.post(`${campground_api}`, values);
-    console.log(response);
+    const create = await axios.post(`${campground_api}`, values);
+    const uploadimg = await axios.post(`${campground_api}/presignedurl`, {
+      id: create.data.campground._id,
+    });
+    console.log(uploadimg);
   };
-
-  const handleChange = (
-    e: React.ChangeEvent<HTMLSelectElement>,
-    { setFieldValue }: FormikHelpers<NewCampInterface>
-  ) => {
-    const selected = e.target.value;
-    setFieldValue("Province", e.target.value);
-    setSelectedLocation(e.target.value);
-    console.log(selectedLocation);
-  };
+  console.log(files);
   return (
     <main className="flex-1 grid grid-cols-2 mt-10">
       <div className="px-16 py-4 my-10 mx-auto">
@@ -91,193 +85,198 @@ function NewCampground() {
         </header>
         <div>
           <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-            <Form>
-              <div className="form-control w-full">
-                <label htmlFor="" className="label ">
-                  <span className="label-text text-black font-[900] flex">
-                    <GiCampfire size={20} className="mr-2" />
-                    Camp name
-                  </span>
-                </label>
-                <Field
-                  name="CampName"
-                  placeholder="Camp name"
-                  type="text"
-                  className="input input-primary input-bordered input-sm focus:outline-none"
-                />
-              </div>
-
-              <div className="form-control w-full">
-                <label htmlFor="" className="label">
-                  <span className="label-text text-black font-[900] flex">
-                    <HiLocationMarker size={20} className="mr-2" />
-                    Location
-                  </span>
-                </label>
-
-                <div className="grid grid-cols-2 gap-4">
+            {(props) => (
+              <Form>
+                <div className="form-control w-full">
+                  <label htmlFor="" className="label ">
+                    <span className="label-text text-black font-[900] flex">
+                      <GiCampfire size={20} className="mr-2" />
+                      Camp name
+                    </span>
+                  </label>
                   <Field
-                    component="select"
+                    name="CampName"
+                    placeholder="Camp name"
                     type="text"
-                    name="Province"
+                    className="input input-primary input-bordered input-sm focus:outline-none"
+                  />
+                </div>
+
+                <div className="form-control w-full">
+                  <label htmlFor="" className="label">
+                    <span className="label-text text-black font-[900] flex">
+                      <HiLocationMarker size={20} className="mr-2" />
+                      Location
+                    </span>
+                  </label>
+
+                  <div className="grid grid-cols-2 gap-4">
+                    <Field
+                      component="select"
+                      type="text"
+                      name="Province"
+                      list="provinces"
+                      placeholder="Province"
+                      className="select select-sm select-primary mb-2"
+                      onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+                        props.setFieldValue("Province", e.currentTarget.value);
+                        setSelectedProvince(e.currentTarget.value);
+                      }}
+                    >
+                      {provinceName.map((name) => (
+                        <option key={name} value={name}>
+                          {name}
+                        </option>
+                      ))}
+                    </Field>
+
+                    <Field
+                      component="select"
+                      type="text"
+                      name="CityOrMunicipalities"
+                      list="CityOrMunicipalities"
+                      placeholder="City or Municipality"
+                      className="select select-sm select-primary"
+                    >
+                      {provinceCity?.map((city) => (
+                        <option key={city} value={city}>
+                          {city}
+                        </option>
+                      ))}
+                    </Field>
+                  </div>
+                  <Field
+                    type="text"
+                    name="Address"
                     id=""
-                    list="provinces"
-                    placeholder="Province"
-                    className="select select-sm select-primary mb-2"
-                  >
-                    {provinceName.map((name) => (
-                      <option key={name} value={name}>
-                        {name}
-                      </option>
-                    ))}
-                  </Field>
+                    placeholder="Address"
+                    className="input input-sm input-primary"
+                  />
+                </div>
 
+                <div className="w-full">
+                  <label htmlFor="" className="label">
+                    <span className="label-text text-black font-[900]">Description</span>
+                  </label>
                   <Field
-                    component="select"
-                    type="text"
-                    name="CityOrMunicipalities"
-                    list="CityOrMunicipalities"
-                    placeholder="City or Municipality"
-                    className="select select-sm select-primary"
-                  >
-                    {provinceCity?.map((city) => (
-                      <option key={city} value={city}>
-                        {city}
-                      </option>
-                    ))}
-                  </Field>
-                </div>
-                <Field
-                  type="text"
-                  name="Address"
-                  id=""
-                  placeholder="Address"
-                  className="input input-sm input-primary"
-                />
-              </div>
-
-              <div className="w-full">
-                <label htmlFor="" className="label">
-                  <span className="label-text text-black font-[900]">Description</span>
-                </label>
-                <Field
-                  component="textarea"
-                  name="Description"
-                  placeholder="A short or brief description about the campsite."
-                  id=""
-                  cols={30}
-                  rows={2}
-                  className="textarea textarea-bordered textarea-primary w-full focus:outline-none resize-none"
-                />
-              </div>
-
-              <div className="grid grid-cols-2 mb-4">
-                <div>
-                  <label htmlFor="" className="label">
-                    <span className="label-text text-black font-[900]">Activities</span>
-                  </label>
-                  <div className="form-control mb-1">
-                    <label htmlFor="" className="flex">
-                      <Field
-                        type="checkbox"
-                        name="Activities"
-                        value="Hiking"
-                        className="checkbox checkbox-primary checkbox-xs mr-3"
-                      />
-                      <span className="text-[12px] flex">
-                        <GiHiking size={16} className="mr-2" />
-                        <span>HIKING</span>
-                      </span>
-                    </label>
-                  </div>
-
-                  <div className="form-control mb-2">
-                    <label htmlFor="" className="flex">
-                      <Field
-                        type="checkbox"
-                        name="Activities"
-                        value="Fishing"
-                        className="checkbox checkbox-primary checkbox-xs mr-3"
-                      />
-                      <span className="text-[12px] flex">
-                        <GiBoatFishing size={16} className="mr-2" />
-                        <span>FISHING</span>
-                      </span>
-                    </label>
-                  </div>
-
-                  <div className="form-control">
-                    <label htmlFor="" className="flex">
-                      <Field
-                        type="checkbox"
-                        name="Activities"
-                        value="Swimming"
-                        className="checkbox checkbox-primary checkbox-xs mr-3"
-                      />
-                      <span className="text-[12px] flex">
-                        <TbSwimming size={16} className="mr-2" />
-                        <span>SWIMMING</span>
-                      </span>
-                    </label>
-                  </div>
+                    component="textarea"
+                    name="Description"
+                    placeholder="A short or brief description about the campsite."
+                    id=""
+                    cols={30}
+                    rows={2}
+                    className="textarea textarea-bordered textarea-primary w-full focus:outline-none resize-none"
+                  />
                 </div>
 
-                <div>
-                  <label htmlFor="" className="label">
-                    <span className="label-text text-black font-[900]">Amenities</span>
-                  </label>
-                  <div className="form-control mb-1">
-                    <label htmlFor="" className="flex">
-                      <Field
-                        type="checkbox"
-                        name="Amenities"
-                        value="Campfire"
-                        className="checkbox checkbox-primary checkbox-xs mr-3"
-                      />
-                      <span className="text-[12px] flex">
-                        <GiCampfire size={16} className="mr-2" />
-                        <span>CAMPFIRE</span>
-                      </span>
+                <div className="grid grid-cols-2 mb-4">
+                  <div>
+                    <label htmlFor="" className="label">
+                      <span className="label-text text-black font-[900]">Activities</span>
                     </label>
+                    <div className="form-control mb-1">
+                      <label htmlFor="" className="flex">
+                        <Field
+                          type="checkbox"
+                          name="Activities"
+                          value="Hiking"
+                          className="checkbox checkbox-primary checkbox-xs mr-3"
+                        />
+                        <span className="text-[12px] flex">
+                          <GiHiking size={16} className="mr-2" />
+                          <span>HIKING</span>
+                        </span>
+                      </label>
+                    </div>
+
+                    <div className="form-control mb-2">
+                      <label htmlFor="" className="flex">
+                        <Field
+                          type="checkbox"
+                          name="Activities"
+                          value="Fishing"
+                          className="checkbox checkbox-primary checkbox-xs mr-3"
+                        />
+                        <span className="text-[12px] flex">
+                          <GiBoatFishing size={16} className="mr-2" />
+                          <span>FISHING</span>
+                        </span>
+                      </label>
+                    </div>
+
+                    <div className="form-control">
+                      <label htmlFor="" className="flex">
+                        <Field
+                          type="checkbox"
+                          name="Activities"
+                          value="Swimming"
+                          className="checkbox checkbox-primary checkbox-xs mr-3"
+                        />
+                        <span className="text-[12px] flex">
+                          <TbSwimming size={16} className="mr-2" />
+                          <span>SWIMMING</span>
+                        </span>
+                      </label>
+                    </div>
                   </div>
 
-                  <div className="form-control mb-2">
-                    <label htmlFor="" className="flex">
-                      <Field
-                        type="checkbox"
-                        name="Amenities"
-                        value="Shower"
-                        className="checkbox checkbox-primary checkbox-xs mr-3"
-                      />
-                      <span className="text-[12px] flex">
-                        <GiShower size={16} className="mr-2" />
-                        <span>SHOWER</span>
-                      </span>
+                  <div>
+                    <label htmlFor="" className="label">
+                      <span className="label-text text-black font-[900]">Amenities</span>
                     </label>
-                  </div>
-                  <div className="form-control">
-                    <label htmlFor="" className="flex">
-                      <Field
-                        type="checkbox"
-                        name="Activities"
-                        value="Electric Hookups"
-                        className="checkbox checkbox-primary checkbox-xs mr-3"
-                      />
-                      <span className="text-[12px] flex">
-                        <GiElectric size={16} className="mr-2" />
-                        <span>ELECTRIC HOOKUP</span>
-                      </span>
-                    </label>
+                    <div className="form-control mb-1">
+                      <label htmlFor="" className="flex">
+                        <Field
+                          type="checkbox"
+                          name="Amenities"
+                          value="Campfire"
+                          className="checkbox checkbox-primary checkbox-xs mr-3"
+                        />
+                        <span className="text-[12px] flex">
+                          <GiCampfire size={16} className="mr-2" />
+                          <span>CAMPFIRE</span>
+                        </span>
+                      </label>
+                    </div>
+
+                    <div className="form-control mb-2">
+                      <label htmlFor="" className="flex">
+                        <Field
+                          type="checkbox"
+                          name="Amenities"
+                          value="Shower"
+                          className="checkbox checkbox-primary checkbox-xs mr-3"
+                        />
+                        <span className="text-[12px] flex">
+                          <GiShower size={16} className="mr-2" />
+                          <span>SHOWER</span>
+                        </span>
+                      </label>
+                    </div>
+                    <div className="form-control">
+                      <label htmlFor="" className="flex">
+                        <Field
+                          type="checkbox"
+                          name="Activities"
+                          value="Electric Hookups"
+                          className="checkbox checkbox-primary checkbox-xs mr-3"
+                        />
+                        <span className="text-[12px] flex">
+                          <GiElectric size={16} className="mr-2" />
+                          <span>ELECTRIC HOOKUP</span>
+                        </span>
+                      </label>
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="form-control">
-                <button type="submit" className="btn btn-primary">
-                  ADD CAMP
-                </button>
-              </div>
-            </Form>
+                <div className="form-control">
+                  <button type="submit" className="btn btn-primary">
+                    ADD CAMP
+                  </button>
+                </div>
+              </Form>
+            )}
           </Formik>
         </div>
       </div>
