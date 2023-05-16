3dee000c601bec6c32340b79d389420cc835a8ab
R-esentful <raneljohnt@gmail.com>
2023-05-16 16:16:04 +0800

    Updated utils

--- backend/utilities/aws.ts
+++ backend/utilities/aws.ts
@@ -10,14 +10,12 @@ dotenv.config();
  * @param id Gets the id of the campground
  * @returns True if it already exists, if not then it creates the folder.
  */
-export const checkFolder = async (id: string) => {
-  const prefix = `campground/${id}/`;
-
+export const checkFolder = async (id: string, location: string) => {
+  const prefix = `${location}/${id}/`;
   const command = new ListObjectsV2Command({ Bucket: bucket, Prefix: prefix, MaxKeys: 1 });
   const response = await s3.send(command);
-  const objects = response.Contents;
 
-  const folderExists = objects!.length > 0;
+  const folderExists = response.KeyCount && response.KeyCount > 0;
   if (!folderExists) {
     const command = new PutObjectCommand({ Bucket: bucket, Key: prefix, Body: "" });
     await s3.send(command);
@@ -26,8 +24,13 @@ export const checkFolder = async (id: string) => {
 };
 
 export const uploadImage = async (id: string, imageNo: string, location: string, type: string) => {
-  const key = `campgrounds/${id}/${imageNo}`;
-  const command = new PutObjectCommand({ Bucket: bucket, Key: location, ContentType: type });
+  const imageType = type.replace("image/", "");
+  const key = `${location}/${id}/${imageNo}.${imageType}`;
+  const command = new PutObjectCommand({
+    Bucket: bucket,
+    Key: key,
+    ContentType: type,
+  });
   const signedURL = await getSignedUrl(s3, command, { expiresIn: 3600 });
-  return signedURL;
+  return { signedURL, key, imageType };
 };
