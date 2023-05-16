f16cf2585deb282660b6fbd4d965e9dd23ac27d1
R-esentful <raneljohnt@gmail.com>
2023-05-16 16:15:48 +0800

    Updated config

--- backend/config/awsconfig.ts
+++ backend/config/awsconfig.ts
@@ -1,13 +1,17 @@
 import dotenv from "dotenv";
+
 import { S3Client } from "@aws-sdk/client-s3";
 dotenv.config();
 
-const accessKey = process.env.accessKey;
-const secretAccessKey = process.env.secretKey;
+const accessKey = process.env.accessKey!;
+const secretAccessKey = process.env.secretKey!;
+
 const s3Config = {
-  accessKeyId: accessKey,
-  secretAccessKey: secretAccessKey,
   region: "ap-southeast-1",
+  credentials: {
+    accessKeyId: accessKey,
+    secretAccessKey: secretAccessKey,
+  },
 };
 
 export const s3 = new S3Client(s3Config);
