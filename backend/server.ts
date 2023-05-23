83d0ca47731fa13274bdb4c4e440c9be66b8de28
R-esentful <raneljohnt@gmail.com>
2023-05-23 19:20:35 +0800

    Updated server

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
 import { ConnectDB } from "./config/db";
 dotenv.config();
 
@@ -21,6 +22,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 // Routes
 app.use("/api/v1/users", userRoute);
